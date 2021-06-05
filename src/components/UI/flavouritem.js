4ccdd0169cea420eff91a30cc8f5deecb48923da
Your Name <coller.vato@gmail.com>
2021-06-06 00:03:00 +0400

    colors

--- src/components/UI/flavouritem.js
+++ src/components/UI/flavouritem.js
@@ -1,17 +1,20 @@
 const FlavourItem = ({item}) =>{
-  const {name, img, slogan, price} = item;
+  const {name, img, slogan,background, price} = item;
   const {img1} = item.fruit;
   return <>
     <div className="flavour_item">
-      <div className="fruit-image">
-        <img src={img1} alt="/" />
+      <div className={`bg ${background}`}></div>
+      <div className="flavour-block">
+        <div className="fruit-image">
+          <img src={img1} alt="/" />
+        </div>
+        <div className="fl-poster-div">
+          <img className="fl-poster" src={img} alt="/" />
+        </div>
+        <h2>{name}</h2>
+        <p>{slogan}</p>
+        <h4>Price: {price}</h4>
       </div>
-      <div className="fl-poster-div">
-        <img className="fl-poster" src={img} alt="/" />
-      </div>
-      <h2>{name}</h2>
-      <p>{slogan}</p>
-      <h4>Price: {price}</h4>
     </div>
   </>
 }
